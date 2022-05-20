45d94f1d7f3911916d990e0d7a0bc464fae77397
BuildTools <unconfigured@null.spigotmc.org>
2022-05-20 15:16:34 +0200

    start

---
+++ app/src/main/java/me/milos/murin/cubehelper/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.milos.murin.cubehelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

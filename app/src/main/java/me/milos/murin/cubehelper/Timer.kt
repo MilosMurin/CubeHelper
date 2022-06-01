a2114756597fbd28c4dd5b31db10af4fa27bccb7
BuildTools <unconfigured@null.spigotmc.org>
2022-06-01 19:08:15 +0200

    Timer working

--- app/src/main/java/me/milos/murin/cubehelper/Timer.kt
+++ app/src/main/java/me/milos/murin/cubehelper/Timer.kt
@@ -1,9 +1,64 @@
 package me.milos.murin.cubehelper
 
-class Timer(var minutes: Int = 0, var seconds: Int = 0, var miliSeconds: Int = 0) {
+import android.os.Handler
+import android.os.Looper
+import android.widget.TextView
 
+class Timer(private var time: Long = 0, private val timer: TextView) {
+
+    private var timerRunning: Boolean = false
+    private var handler: Handler = Handler(Looper.getMainLooper())
+
+    private lateinit var runnable: Runnable
+    init {
+        runnable = Runnable {
+            if (timerRunning) {
+                timer.text = getDiff(System.currentTimeMillis())
+
+                handler.postDelayed(runnable, 1)
+            }
+        }
+    }
+
+    fun startTimer() {
+        timerRunning = true
+        resetTimer()
+        handler.postDelayed(runnable, 1)
+    }
+
+    fun stopTimer() {
+        if (timerRunning) {
+            timerRunning = false
+            handler.removeCallbacks(runnable)
+            timer.text = getDiff(System.currentTimeMillis())
+        }
+    }
+
+    fun resetTimer() {
+        time = System.currentTimeMillis()
+    }
+
+    fun getDiff(oTime: Long): String {
+        return getTimeString(oTime - time)
+    }
     override fun toString(): String {
-        return String.format("%02d:%02d,%03d", minutes, seconds, miliSeconds)
+        return getTimeString(time)
+    }
+
+
+    companion object {
+        fun getTimeString(time: Long): String {
+            val miliSeconds = time % 1000
+            val seconds = (time / 1000) % 60
+            val minutes = ((time / 1000) / 60) % 60
+
+            return String.format("%02d:%02d,%03d", minutes, seconds, miliSeconds)
+        }
+
+
+        fun getNullTime(): String {
+            return getTimeString(0)
+        }
     }
 
 
--- app/src/main/java/me/milos/murin/cubehelper/fragments/TimerFragment.kt
+++ app/src/main/java/me/milos/murin/cubehelper/fragments/TimerFragment.kt
@@ -1,24 +1,24 @@
 package me.milos.murin.cubehelper.fragments
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import me.milos.murin.cubehelper.R
 import me.milos.murin.cubehelper.Timer
+import me.milos.murin.cubehelper.Timer.Companion.getNullTime
 import me.milos.murin.cubehelper.databinding.FragmentTimerBinding
 
 class TimerFragment : Fragment() {
 
-    private var timerRunning: Boolean = false
-    private var timer: Timer = Timer()
-    private var held: Int = 0
+    private lateinit var timer: Timer
     private var start: Boolean = false
-
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -26,18 +26,73 @@ class TimerFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentTimerBinding>(
             inflater, R.layout.fragment_timer, container, false)
 
+        timer = Timer(timer = binding.timer)
+
+
         binding.scramble.text = getString(R.string.scramble, generateScramble())
 
         setTimes(binding)
 
+
+
+        binding.timerBack.setOnClickListener { view: View ->
+            run {
+                timer.stopTimer()
+                binding.timerBack.setBackgroundColor(Color.WHITE)
+                changeVisibility(binding, true)
+                binding.tapAndHold.text = getString(R.string.timer_info)
+                // TODO: Saving times, calculating averages
+            }
+        }
+
+        binding.timerBack.setOnLongClickListener {
+            if (!start) {
+                start = true
+                binding.timerBack.setBackgroundColor(Color.YELLOW)
+            }
+            return@setOnLongClickListener true
+        }
+
+        binding.timerBack.setOnTouchListener { view, event ->
+            run {
+                if (event.action == MotionEvent.ACTION_UP) {
+                    if (start) {
+                        start = false
+                        binding.timerBack.setBackgroundColor(Color.GREEN)
+                        timer.startTimer()
+                        changeVisibility(binding, false)
+                        binding.tapAndHold.text = getString(R.string.timer_running_info)
+                    }
+                }
+                return@run view?.onTouchEvent(event) ?: true
+            }
+        }
+
         return binding.root
+    }
+
+    private fun generateScramble(): String {
+        // TODO("Generate scrambles")
+        return "TODO: Generate scrambles"
+    }
+
+    private fun changeVisibility(binding: FragmentTimerBinding, to: Boolean) {
+        binding.scramble.isVisible = to
+        binding.lastSolve.isVisible = to
+        binding.bestSolve.isVisible = to
+        binding.worstSolve.isVisible = to
+        binding.averageOf5.isVisible = to
+        binding.averageOf10.isVisible = to
+        binding.averageOf12.isVisible = to
+        binding.averageOf100.isVisible = to
+        binding.averageOf1000.isVisible = to
     }
 
     private fun setTimes(binding: FragmentTimerBinding) {
         binding.timer.text = timer.toString()
-        binding.lastSolve.text = getString(R.string.last_solve, Timer())
-        binding.bestSolve.text = getString(R.string.best_solve, Timer())
-        binding.worstSolve.text = getString(R.string.worst_solve, Timer())
+        binding.lastSolve.text = getString(R.string.last_solve, getNullTime())
+        binding.bestSolve.text = getString(R.string.best_solve, getNullTime())
+        binding.worstSolve.text = getString(R.string.worst_solve, getNullTime())
 
         binding.averageOf5.text = averageString(5)
         binding.averageOf10.text = averageString(10)
@@ -46,13 +101,8 @@ class TimerFragment : Fragment() {
         binding.averageOf1000.text = averageString(1000)
     }
 
-    private fun generateScramble(): String {
-        // TODO("Generate scrambles")
-        return "R"
-    }
-
     private fun averageString(of: Int): String {
-        return getString(R.string.average_of, of, Timer())
+        return getString(R.string.average_of, of, getNullTime())
     }
 
 }
